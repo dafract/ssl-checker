2edc2d261eed2da30e6f0dd42315c52b31558a1b
dafract <dafract@gmail.com>
2023-05-19 13:09:49 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ssl-cheker
+
+## How to usage
+run the following command
+```sh
+$ bash ssl-check www.example.com
+```
+
+then you will get the following response
+```
+93.184.216.34
+subject= /C=US/ST=California/L=Los Angeles/O=Internet\xC2\xA0Corporation\xC2\xA0for\xC2\xA0Assigned\xC2\xA0Names\xC2\xA0and\xC2\xA0Numbers/CN=www.example.org
+notBefore=Jan 13 00:00:00 2023 GMT
+notAfter=Feb 13 23:59:59 2024 GMT
+    Verify return code: 0 (ok)
+remaining: 270 days
+```
+
---
+++ ssl-check.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+FQDN=$1
+if [ $# -gt 1 ]; then
+    ips=$2
+else
+    ips=$(dig $FQDN +short | grep -v '\.$')
+fi
+for ip in $ips
+do
+    openssl_output=$(openssl s_client -connect $ip:443 -servername $FQDN -host $FQDN -showcerts < /dev/null 2> /dev/null)
+    enddate=$(echo "$openssl_output" | openssl x509 -noout -enddate | cut -d= -f2 | sed 's/Jan/01/;s/Feb/02/;s/Mar/03/;s/Apr/04/;s/May/05/;s/Jun/06/;s/Jul/07/;s/Aug/08/;s/Sep/09/;s/Oct/10/;s/Nov/11/;s/Dec/12/')
+    days=$((($(date -j -f '%m %d %H:%M:%S %Y %Z' "$enddate" '+%s') - $(TZ=GMT date '+%s')) / (60 * 60 * 24)))
+    echo $ip
+    echo "$openssl_output" | openssl x509 -noout -subject -dates
+    echo "$openssl_output" | grep 'Verify return code:'
+    echo "remaining: $days days"
+    echo 
+done
+
